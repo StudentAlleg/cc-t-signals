68bc46999a182812f908a6129dc827bad1d20442
ocpeters <ocpeters@ucsc.edu>
2022-04-16 00:54:58 -0700

    wrking on both signal and signal_control, adding rednet functionality

--- signal.lua
+++ signal.lua
@@ -1,22 +1,18 @@
 
-signal_aspect = {
-    "RED",
-    "FLASHING_RED",
-    "YELLOW",
-    "FLASHING_YELLOW",
-    "GREEN",
-}
-
 rednet = require("Rednet")
 os = require("os")
+settings = require("settings")
+
 Signal = {id = 0, aspect = 0}
 Signal.__index = ConnectedComponents
 
-function Signal:new(o, id)
+function Signal:new(o)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
-    self.id = id or 0
+    if settings.get("id") == nil then
+        settings.set("id", 0)
+    self.id = settings.get("id")
     self.aspect = 0
     return o
 end
@@ -29,6 +25,11 @@ function Signal:getAspect()
     return self.aspect()
 end
 
+function Signal:setId(id)
+    self.id = id
+    settings.set("id", id)
+
+
 function Signal:getId()
     return self.id
 end
@@ -37,8 +38,20 @@ function Signal:update()
     ---check self.aspect and then set the proper redstone based on that
 end
 
+function init()
+    
+    
+
 function signal_listen()
     while true:
-        id, rednet.receive("signals")
+        local id, message = rednet.receive("signals")
+        if id == "signal_controller"
 
     end
+
+signal = Signal:new(nil)
+rednet.open("back")
+while ~pcall(rednet.host, "signal", self.id) do
+    signal.setID(signal.getID() + 1)
+end
+
--- signal_controller.lua
+++ signal_controller.lua
@@ -1,10 +1,16 @@
-
+rednet = require("rednet")
 signal_id = {}
 
-function setSignal(id, aspect)
-    rednet.send(signal_id)
+
+
+function init()
+
+
+
+function setSignalAspect(id, aspect)
+    rednet.send(signal_id, "set aspect " .. aspect)
+end
 
 function getSignal(id, aspect)
 
 function matchIDs
-
