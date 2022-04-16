7bdf7a53e95966841df93f00847bc12151278691
ocpeters <ocpeters@ucsc.edu>
2022-04-16 01:04:50 -0700

    end of day 4/15

--- signal_controller.lua
+++ signal_controller.lua
@@ -3,14 +3,13 @@ signal_id = {}
 
 
 
-function init()
-
-
-
 function setSignalAspect(id, aspect)
     rednet.send(signal_id, "set aspect " .. aspect)
 end
 
 function getSignal(id, aspect)
 
-function matchIDs
+rednet.open("back")
+rednet.host("signals", "signal_controller")
+
+for 
